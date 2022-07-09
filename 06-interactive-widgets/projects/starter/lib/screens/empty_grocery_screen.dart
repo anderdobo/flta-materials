b9bcc09cd1efcb6a8c7d1935b4c57285f245be81
Ander Dobo <anderdobo@users.noreply.github.com>
2022-07-08 17:13:11 -0700

    Update

--- 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -4,7 +4,32 @@ class EmptyGroceryScreen extends StatelessWidget {
   const EmptyGroceryScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-// TODO 3: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    // 1
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      // 2
+      child: Center(
+        // 3
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // 1
+            Flexible(
+              // 2
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+              ),
+            ),
+            const Text(
+              'No Groceries',
+              style: TextStyle(fontSize: 21.0),
+            ),
+            // TODO 6: Add empty screen subtitle
+            // TODO 7: Add browse recipes button
+          ],
+        ),
+      ),
+    );
   }
 }
