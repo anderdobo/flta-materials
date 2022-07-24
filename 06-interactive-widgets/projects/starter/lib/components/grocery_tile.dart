18b8a070947e64b3f0f1d0f3e273fd1e498634dd
Ander Dobo <anderdobo@users.noreply.github.com>
2022-07-24 15:36:50 -0700

    Chapter 6 completed

--- 06-interactive-widgets/projects/starter/lib/components/grocery_tile.dart
+++ 06-interactive-widgets/projects/starter/lib/components/grocery_tile.dart
@@ -20,14 +20,89 @@ class GroceryTile extends StatelessWidget {
         super(key: key);
   @override
   Widget build(BuildContext context) {
-// TODO 21: Change this Widget
-    return Container(
+    return SizedBox(
       height: 100.0,
-// TODO 20: Replace this color
-      color: Colors.red,
+      // 1
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // 2
+          Container(width: 5.0, color: item.color),
+          // 3
+          const SizedBox(width: 16.0),
+          // 4
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // 5
+              Text(
+                item.name,
+                style: GoogleFonts.lato(
+                    decoration: textDecoration,
+                    fontSize: 21.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 4.0),
+              buildDate(),
+              const SizedBox(height: 4.0),
+              buildImportance(),
+            ],
+          ),
+          // 6
+          Row(
+            children: [
+              // 7
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                    decoration: textDecoration, fontSize: 21.0),
+              ),
+              // 8
+              buildCheckbox(),
+            ],
+          ),
+        ],
+      ),
     );
   }
-// TODO: Add BuildImportance()
-// TODO: Add buildDate()
-// TODO: Add buildCheckbox()
+
+  Widget buildImportance() {
+    if (item.importance == Importance.low) {
+      return Text('Low', style: GoogleFonts.lato(decoration: textDecoration));
+    } else if (item.importance == Importance.medium) {
+      return Text('Medium',
+          style: GoogleFonts.lato(
+              fontWeight: FontWeight.w800, decoration: textDecoration));
+    } else if (item.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+          color: Colors.red,
+          fontWeight: FontWeight.w900,
+          decoration: textDecoration,
+        ),
+      );
+    } else {
+      throw Exception('This importance type does not exist');
+    }
+  }
+
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
+
+  Widget buildCheckbox() {
+    return Checkbox(
+      // 1
+      value: item.isComplete,
+      // 2
+      onChanged: onComplete,
+    );
+  }
 }
