7128be15bcaac57d94c4ab5f138410088519a06c
Ander Dobo <anderdobo@users.noreply.github.com>
2023-10-21 22:17:34 -0700

    Update

--- 12-using-a-network-library/projects/starter/lib/network/recipe_service.dart
+++ 12-using-a-network-library/projects/starter/lib/network/recipe_service.dart
@@ -1,9 +1,23 @@
+// 1
+import 'package:chopper/chopper.dart';
+import 'recipe_model.dart';
+import 'model_response.dart';
+import 'model_converter.dart';
+// 2
+const String apiKey = '<Your Key Here>';
+const String apiId = '<Your Id here>';
+// 3
+const String apiUrl = 'https://api.edamam.com';
+// TODO: Add @ChopperApi() here
+
+
+
 import 'dart:developer';
 
 import 'package:http/http.dart';
 
-const String apiKey = '<Your Key>';
-const String apiId = '<your ID>';
+const String apiKey = '13a052bb0ed69c91b4986f72a629cb08';
+const String apiId = '430c5ea4';
 const String apiUrl = 'https://api.edamam.com/search';
 
 class RecipeService {
